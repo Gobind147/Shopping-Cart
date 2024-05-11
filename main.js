f457f3d9926c139ca74c69d2981c8e903b737330
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2024-05-12 00:48:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -30,21 +30,23 @@
     img: "images/img-4.jpg"
  }]
 
+ let basket = [];
 
  let generateShop =()=>{
     return (shop.innerHTML= shopItemsData.map((x)=>{
+    let{id,name, price, desc, img} = x;   
         return `
-        <div class="item">
-                <img width="220" src="images/img-1.jpg" alt="">
+        <div id=product-id-${id} class="item">
+                <img width="220" src=${img} alt="">
                 <div class="details">
-                    <h3>${x.name}</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                    <h3>${name}</h3>
+                    <p>${desc}</p>
                     <div class="price-quantity">
-                     <h2>$ 45</h2>
+                     <h2>$ ${price}</h2>
                      <div class="buttons">
-                        <i class="bi bi-dash-lg"></i>
-                        <div class="quantity">0</div>
-                        <i class="bi bi-plus-lg"></i>
+                        <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                        <div id=${id} class="quantity">0</div>
+                        <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                      </div>       
                     </div>
                 </div>
@@ -54,3 +56,27 @@
  };
 
  generateShop();
+
+ let increment = (id)=>{
+   let selectedItem = id; 
+   let search = basket.find((x)=> x.id === selectedItem.id);
+
+   if(search === undefined){
+      basket.push({
+      id: selectedItem.id,
+      item: 1,
+   });
+   }
+
+   else{
+      search.item +=1;
+   }
+
+   
+   console.log(basket);
+ };
+ let decrement = (id)=>{
+   let selectedItem = id; 
+   console.log(selectedItem.id);
+ };
+ let update = ()=>{};

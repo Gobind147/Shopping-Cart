bf7d0ff1ea1c9458a3d3e226228517b7ef937ff3
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2024-05-12 22:55:13 +0530

    Create Data.js

---
+++ src/Data.js
@@ -0,0 +1,28 @@
+let shopItemsData = [{
+    id:"jkjcbkscksc",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg"
+ },
+ {
+    id:"jkjcmnjnkas",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg"
+ },
+ {
+    id:"jkjcnmsmc",
+    name: "T-Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg"
+ },
+ {
+    id:"jkjcDJHJF",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg"
+ }]

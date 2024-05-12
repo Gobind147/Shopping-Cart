8394e665232249235f83ea42d868896b1849cc60
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2024-05-12 09:25:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -71,26 +71,35 @@
    else{
       search.item +=1;
    }
-
+   localStorage.setItem("data", basket);
    
-   console.log(basket);
+   //console.log(basket);
+   update(selectedItem.id);
  };
  let decrement = (id)=>{
    let selectedItem = id; 
    let search = basket.find((x)=> x.id === selectedItem.id);
 
-   if(search === undefined){
-      basket.push({
-      id: selectedItem.id,
-      item: 1,
-   });
-   }
+   if(search.item === 0) 
+   return;
 
    else{
       search.item -=1;
    }
 
    
-   console.log(basket);
+   //console.log(basket);
+   update(selectedItem.id);
  };
- let update = ()=>{};
+ let update = (id)=>{
+   let search = basket.find((x)=>x.id ===id);
+   //console.log(search.item);
+   document.getElementById(id).innerHTML = search.item;
+   calculation();
+ }; 
+
+ let calculation =()=> {
+   let cartIcon = document.getElementById("cartAmount");
+   cartIcon.innerHTML = basket.map((x)=>x.item).reduce((x,y)=>x+y,0);
+   
+ };

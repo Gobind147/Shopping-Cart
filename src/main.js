a03970de0c52bf9c6c52812d60b05dd0249683bc
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2024-05-12 22:53:58 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,105 +1,107 @@
- let shop  = document.getElementById('shop');
+let shop = document.getElementById("shop");
 
+/**
+ * ! Basket to hold all the selected items
+ * ? the getItem part is retrieving data from the local storage
+ * ? if local storage is blank, basket becomes an empty array
+ */
 
- let shopItemsData = [{
-    id:"jkjcbkscksc",
-    name: "Casual Shirt",
-    price: 45,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-1.jpg"
- },
- {
-    id:"jkjcmnjnkas",
-    name: "Office Shirt",
-    price: 100,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-2.jpg"
- },
- {
-    id:"jkjcnmsmc",
-    name: "T-Shirt",
-    price: 25,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-3.jpg"
- },
- {
-    id:"jkjcDJHJF",
-    name: "Mens Suit",
-    price: 300,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-4.jpg"
- }]
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
- let basket = [];
+/**
+ * ! Generates the shop with product cards composed of
+ * ! images, title, price, buttons, description
+ */
 
- let generateShop =()=>{
-    return (shop.innerHTML= shopItemsData.map((x)=>{
-    let{id,name, price, desc, img} = x;   
-        return `
-        <div id=product-id-${id} class="item">
-                <img width="220" src=${img} alt="">
-                <div class="details">
-                    <h3>${name}</h3>
-                    <p>${desc}</p>
-                    <div class="price-quantity">
-                     <h2>$ ${price}</h2>
-                     <div class="buttons">
-                        <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                        <div id=${id} class="quantity">0</div>
-                        <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
-                     </div>       
-                    </div>
-                </div>
-            </div>
-        `;
-    }).join(""));
- };
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, name, desc, img, price } = x;
+      let search = basket.find((y) => y.id === id) || [];
+      return `
+    <div id=product-id-${id} class="item">
+      <img width="220" src=${img} alt="">
+      <div class="details">
+        <h3>${name}</h3>
+        <p>${desc}</p>
+        <div class="price-quantity">
+          <h2>$ ${price} </h2>
+          <div class="buttons">
+            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+            <div id=${id} class="quantity">${
+        search.item === undefined ? 0 : search.item
+      }</div>
+            <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+          </div>
+        </div>
+      </div>
+  </div>
+    `;
+    })
+    .join(""));
+};
 
- generateShop();
+generateShop();
 
- let increment = (id)=>{
-   let selectedItem = id; 
-   let search = basket.find((x)=> x.id === selectedItem.id);
+/**
+ * ! used to increase the selected product item quantity by 1
+ */
 
-   if(search === undefined){
-      basket.push({
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
       id: selectedItem.id,
       item: 1,
-   });
-   }
+    });
+  } else {
+    search.item += 1;
+  }
 
-   else{
-      search.item +=1;
-   }
-   localStorage.setItem("data", basket);
-   
-   //console.log(basket);
-   update(selectedItem.id);
- };
- let decrement = (id)=>{
-   let selectedItem = id; 
-   let search = basket.find((x)=> x.id === selectedItem.id);
+  console.log(basket);
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
 
-   if(search.item === 0) 
-   return;
+/**
+ * ! used to decrease the selected product item quantity by 1
+ */
 
-   else{
-      search.item -=1;
-   }
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
 
-   
-   //console.log(basket);
-   update(selectedItem.id);
- };
- let update = (id)=>{
-   let search = basket.find((x)=>x.id ===id);
-   //console.log(search.item);
-   document.getElementById(id).innerHTML = search.item;
-   calculation();
- }; 
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
 
- let calculation =()=> {
-   let cartIcon = document.getElementById("cartAmount");
-   cartIcon.innerHTML = basket.map((x)=>x.item).reduce((x,y)=>x+y,0);
-   
- };
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  console.log(basket);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+/**
+ * ! To update the digits of picked items on each item card
+ */
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
+
+/**
+ * ! To calculate total amount of selected Items
+ */
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
